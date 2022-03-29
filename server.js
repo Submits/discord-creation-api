3993db576028b73efe381e8e318af51959b6798c
Submits <101422599+Submits@users.noreply.github.com>
2022-03-29 22:06:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const app = express();
 
 
-   function snowflakeToDate(snowflake) {
+function snowflakeToDate(snowflake) {
     const dateBits = Number(BigInt.asUintN(64, snowflake) >> BigInt(unescape('%32%32')));
     return new Date(dateBits + 1420070400000);
 }
 
-  app.get('/:id',(req, res) => {
-res.setHeader('Access-Control-Allow-Origin', '*');
- let id = req.params.id
- res.send({date: snowflakeToDate(id)})
+app.get('/:id', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let id = req.params.id
+    res.send({
+        date: snowflakeToDate(id)
+    })
 })
 
- 
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Ready")
+    console.log("Ready")
 })
